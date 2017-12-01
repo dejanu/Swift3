c9d8b5bb5e530a79456648568955fc78c336a56a
Dej <dejanualex@yahoo.com>
2017-12-02 01:14:22 +0200

    first app

---
+++ appone/Homework/Homework/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Homework
+//
+//  Created by OLPS RO TV on 12/2/17.
+//  Copyright © 2017 OLPS RO TV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
