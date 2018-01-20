b69bf4c0bf277bb57e7fcf54f3538b82d6694e30
Dej <dejanualex@yahoo.com>
2018-01-20 17:47:08 +0200

    Navigation controller

--- StoryboardAndScenes/StoryboardAndScenes/ViewController.swift
+++ StoryboardAndScenes/StoryboardAndScenes/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.white
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+//class
 }
 
